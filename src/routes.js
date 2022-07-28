5f6008f87baac2b822de450472d539bf8bf62890
name <name@email.com>
2022-07-28 12:09:03 +0200

    Fix getAttr when no review

--- src/routes.js
+++ src/routes.js
@@ -31,7 +31,7 @@ exports.SEARCH_PAGE = async (page, request, query, requestQueue, maxPostCount, e
     // eslint-disable-next-line no-shadow
     const data = await page.evaluate(
         (maxPostCount, query, savedItems) => {
-            
+
             // nodes with items
             let results = Array.from(document.querySelectorAll('.sh-dlr__list-result'));
             if (results.length === 0) results = Array.from(document.querySelectorAll('.sh-dlr__content'));
@@ -48,7 +48,7 @@ exports.SEARCH_PAGE = async (page, request, query, requestQueue, maxPostCount, e
                 const item = results[i];
                 // KEYS OF OUTPUT OBJ
 
-                console.log("Product", i)
+                // console.log("Product", i)
 
                 const type = 'result'
 
@@ -72,36 +72,44 @@ exports.SEARCH_PAGE = async (page, request, query, requestQueue, maxPostCount, e
                 // const idArray = productLink ? productLink.split('?')[0].split('/') : null;
                 // const shoppingId = idArray ? idArray[idArray.length - 1] : null;
 
-                const reviewStr = item.querySelector('div.tDoYpc div').getAttribute('aria-label').replace(/,/g, '.')
-                const numbers = reviewStr.match(/\d+\.?\d*/g)
-                const n1 = parseFloat(numbers[0])
-                const n2 = parseFloat(numbers[1])
+                console.log("Item ;", item)
+                console.log("Item div ;", item.querySelector('div.tDoYpc div'))
 
                 let reviewsScore = 0
                 let reviewsCount = 0
-                if (n1 > 5 && n2 <= 5) {
 
-                    reviewsScore = n1
-                    reviewsCount = n2
+                if (item.querySelector('div.tDoYpc div')) {
 
-                } else if (n2 > 5 && n1 <= 5) {
+                    const reviewStr = item.querySelector('div.tDoYpc div').getAttribute('aria-label').replace(/,/g, '.')
+                    const numbers = reviewStr.match(/\d+\.?\d*/g)
+                    const n1 = parseFloat(numbers[0])
+                    const n2 = parseFloat(numbers[1])
 
-                    reviewsScore = n2
-                    reviewsCount = n1
+                    if (n1 > 5 && n2 <= 5) {
 
-                } else {
-                    // L'un des 2 n'est pas < 5 ... donc doute ...
-                    // Seule possibilité : Les 2 sont < 5, puisque la note sur 5 ... ne peut être > 5 ... genious
-
-                    // Le nb reviews est le 
-                    const elemText = item.querySelector('div.tDoYpc div div').textContent.replace(/\s+/g, '')
-                    if (elemText.indexOf(n1) > -1) {
                         reviewsScore = n2
                         reviewsCount = n1
-                    } else {
+
+                    } else if (n2 > 5 && n1 <= 5) {
+
                         reviewsScore = n1
                         reviewsCount = n2
+
+                    } else {
+                        // L'un des 2 n'est pas < 5 ... donc doute ...
+                        // Seule possibilité : Les 2 sont < 5, puisque la note sur 5 ... ne peut être > 5 ... genious
+
+                        // Le nb reviews est le 
+                        const elemText = item.querySelector('div.tDoYpc div div').textContent.replace(/\s+/g, '')
+                        if (elemText.indexOf(n1) > -1) {
+                            reviewsScore = n2
+                            reviewsCount = n1
+                        } else {
+                            reviewsScore = n1
+                            reviewsCount = n2
+                        }
                     }
+
                 }
 
                 // FINAL OUTPUT OBJ
@@ -129,14 +137,14 @@ exports.SEARCH_PAGE = async (page, request, query, requestQueue, maxPostCount, e
             // nodes with items
             let adContainer = document.querySelector('.GhTN2e')
             let ads = Array.from(adContainer.querySelectorAll('.KZmu8e'));
-            
+
             // ITERATING NODES TO GET ADS
             for (let i = 0; i < ads.length; i++) {
                 // Please pay attention that "merchantMetrics" and "reviewsLink" were removed from the  "SEARCH" page.
                 const item = ads[i];
                 // KEYS OF OUTPUT OBJ
 
-                console.log("Ad", i)
+                // console.log("Ad", i)
 
                 const type = 'ad'
 
@@ -160,35 +168,42 @@ exports.SEARCH_PAGE = async (page, request, query, requestQueue, maxPostCount, e
                 // const idArray = productLink ? productLink.split('?')[0].split('/') : null;
                 // const shoppingId = idArray ? idArray[idArray.length - 1] : null;
 
-                const reviewStr = item.querySelector('div.U6puSd div').getAttribute('aria-label')
-                const numbers = reviewStr.match(/\d+\.?\d*/g)
-                const n1 = parseFloat(numbers[0])
-                const n2 = parseFloat(numbers[1])
-
                 let reviewsScore = 0
                 let reviewsCount = 0
-                if (n1 > 5 && n2 <= 5) {
 
-                    reviewsScore = n1
-                    reviewsCount = n2
+                console.log("Ad ;", item)
+                console.log("Ad div ;", item.querySelector('div.U6puSd div'))
 
-                } else if (n2 > 5 && n1 <= 5) {
+                if (item.querySelector('div.U6puSd div')) {
 
-                    reviewsScore = n2
-                    reviewsCount = n1
+                    const reviewStr = item.querySelector('div.U6puSd div').getAttribute('aria-label')
+                    const numbers = reviewStr.match(/\d+\.?\d*/g)
+                    const n1 = parseFloat(numbers[0])
+                    const n2 = parseFloat(numbers[1])
 
-                } else {
-                    // L'un des 2 n'est pas < 5 ... donc doute ...
-                    // Seule possibilité : Les 2 sont < 5, puisque la note sur 5 ... ne peut être > 5 ... genious
+                    if (n1 > 5 && n2 <= 5) {
 
-                    // Le nb reviews est le 
-                    const elemText = item.querySelector('div.U6puSd div span').textContent.replace(/\s+/g, '')
-                    if (elemText.indexOf(n1) > -1) {
                         reviewsScore = n2
                         reviewsCount = n1
-                    } else {
+
+                    } else if (n2 > 5 && n1 <= 5) {
+
                         reviewsScore = n1
                         reviewsCount = n2
+
+                    } else {
+                        // L'un des 2 n'est pas < 5 ... donc doute ...
+                        // Seule possibilité : Les 2 sont < 5, puisque la note sur 5 ... ne peut être > 5 ... genious
+
+                        // Le nb reviews est le 
+                        const elemText = item.querySelector('div.U6puSd div span').textContent.replace(/\s+/g, '')
+                        if (elemText.indexOf(n1) > -1) {
+                            reviewsScore = n2
+                            reviewsCount = n1
+                        } else {
+                            reviewsScore = n1
+                            reviewsCount = n2
+                        }
                     }
                 }
 
@@ -216,7 +231,6 @@ exports.SEARCH_PAGE = async (page, request, query, requestQueue, maxPostCount, e
 
                 data.push(output);
             }
-
 
             return data;
         },
